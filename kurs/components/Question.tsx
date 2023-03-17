a0b908b187fca4d79eedc63bded55bf1dffa8864
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-03-17 11:18:52 +0100

    complete

--- kurs/components/Question.tsx
+++ kurs/components/Question.tsx
@@ -10,14 +10,22 @@ interface input {
 
 function Question({ question, answers, correctIndex, activeIndex, setActiveindex}: input) {
     const [correct, setCorrect] = useState(false)
+    const [wrong, setWrong] = useState(false)
+
     useEffect(() => {
         setCorrect(false)
+        setWrong(false)
     }, [activeIndex])
     
     function checkCorrect(i: number) {
         if(i == correctIndex) {
             console.log("Correct Answer")
             setCorrect(true)
+            setWrong(false)
+        }
+        else {
+            setCorrect(false)
+            setWrong(true)
         }
     }
 
@@ -26,9 +34,12 @@ function Question({ question, answers, correctIndex, activeIndex, setActiveindex
             <h1>{question}</h1>
             {answers.map((answer, i) => <p onClick={() => checkCorrect(i)}>{answer}</p>)}
             <div>
-                {correct ? <p>CORRECT ANSWER!!!</p> : <></>}
+                {correct ? <p>CORRECT ANSWER!!!</p>: <></>}
+                {wrong ? <p>WRONG ANSWER!!!</p> : <></>}
             </div>
-            <button onClick={() => setActiveindex(activeIndex + 1)}>Next question</button>
+                {correct ?
+                <button onClick={() => setActiveindex(activeIndex + 1)}>Next question</button>
+                : <></>}
         </div>
     )
 }
--- kurs/pages/oppgave3/index.tsx
+++ kurs/pages/oppgave3/index.tsx
@@ -4,16 +4,29 @@ import Question from '@/components/Question'
 
 function index() {
     const [activeIndex, setActiveIndex] = useState(0)
+    const [complete, setComplete] = useState(false)
+
+    useEffect(() => {
+        if (activeIndex+2 > Data.questions.length) {
+            setComplete(true)
+        }
+    }, [activeIndex])
+    
+
     return (
         <div>
             <h1>Quiz</h1>
-            <Question
-                question={Data.questions[activeIndex].question}
-                answers={Data.questions[activeIndex].answers}
-                correctIndex={Data.questions[activeIndex].correctIndex}
-                activeIndex={activeIndex}
-                setActiveindex={setActiveIndex}
-            />
+            <h2>{activeIndex+1} / {Data.questions.length - 1}</h2>
+            {complete 
+            ? <h1>You are finished!!!</h1>
+            : <Question
+            question={Data.questions[activeIndex].question}
+            answers={Data.questions[activeIndex].answers}
+            correctIndex={Data.questions[activeIndex].correctIndex}
+            activeIndex={activeIndex}
+            setActiveindex={setActiveIndex}
+        />
+            }
         </div>
     )
 }
